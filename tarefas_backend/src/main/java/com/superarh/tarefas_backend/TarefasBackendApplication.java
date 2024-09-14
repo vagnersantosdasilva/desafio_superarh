6e39b2b8fda01c6094af06e4d6182a80ecae4e25
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2024-09-14 14:35:17 -0300

    primeiro commit

---
+++ tarefas_backend/src/main/java/com/superarh/tarefas_backend/TarefasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.superarh.tarefas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefasBackendApplication.class, args);
+	}
+
+}
---
+++ tarefas_backend/src/test/java/com/superarh/tarefas_backend/TarefasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superarh.tarefas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
